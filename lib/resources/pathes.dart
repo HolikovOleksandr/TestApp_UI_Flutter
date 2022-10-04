84d35ad728c5cd34274200fce2f0ca25fd51379c
Holikov_Oleksandr <sanyagolikov@gmail.com>
2022-10-04 22:16:13 +0300

    add: icon pathes class

---
+++ lib/resources/pathes.dart
@@ -0,0 +1,15 @@
+class AppIcons {
+  static const String airplane = 'assets/icons/airplane.svg';
+  static const String clothes = 'assets/icons/clothes.svg';
+  static const String delivery = 'assets/icons/delivery.svg';
+  static const String filter = 'assets/icons/filter.svg';
+  static const String flower = 'assets/icons/flower.svg';
+  static const String home = 'assets/icons/home.svg';
+  static const String info = 'assets/icons/info.svg';
+  static const String location = 'assets/icons/location.svg';
+  static const String marcetplase = 'assets/icons/marcetplace.svg';
+  static const String more = 'assets/icons/more.svg';
+  static const String qrCode = 'assets/icons/qr_code.svg';
+  static const String shop = 'assets/icons/shop.svg';
+  static const String time = 'assets/icons/time.svg';
+}
