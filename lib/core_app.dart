3811032cc3f46f898bb5fcca483ae7e4677736ff
Holikov_Oleksandr <sanyagolikov@gmail.com>
2022-10-07 13:58:08 +0300

    add: Cashback iten to product description page

--- lib/core_app.dart
+++ lib/core_app.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:test_exercise/router/routes.dart';
 import 'package:test_exercise/src/core/base_widgets/base_statless_widget.dart';
-import 'src/ui/pages/home/components/swipe_to_bank_card.dart';
 
 class CoreApp extends BaseStatelessWidget {
   const CoreApp({Key? key}) : super(key: key);
@@ -13,7 +12,6 @@ class CoreApp extends BaseStatelessWidget {
       debugShowCheckedModeBanner: false,
       unknownRoute: unknownRoute,
       initialRoute: rootRoute,
-      // home: const SwipeToBankCard(),
       navigatorKey: Get.key,
       getPages: routes,
     );
--- lib/src/ui/pages/product_description/product_description_page.dart
+++ lib/src/ui/pages/product_description/product_description_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:test_exercise/resources/colors.dart';
@@ -30,37 +32,41 @@ class ProductDescriptionPage extends BaseStatelessWidget {
         ),
         child: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+            padding: const EdgeInsets.symmetric(vertical: 16),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(height: 16),
                 // IMAGE WITH BACK ICON
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(25),
-                  child: Stack(
-                    children: [
-                      Image.asset(
-                        product.image,
-                        width: double.infinity,
-                        scale: .2,
-                      ),
-                      IconButton(
-                        onPressed: () => Get.back(),
-                        icon: const Icon(
-                          Icons.arrow_back_rounded,
-                          color: AppColor.white,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: Stack(
+                      children: [
+                        Image.asset(
+                          product.image,
+                          width: double.infinity,
+                          scale: .2,
                         ),
-                      ),
-                    ],
+                        IconButton(
+                          onPressed: () => Get.back(),
+                          icon: const Icon(
+                            Icons.arrow_back_rounded,
+                            color: AppColor.white,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 // NAME WITH PRICE AND CASHBACK
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24),
+                  padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Column(
                         mainAxisAlignment: MainAxisAlignment.start,
@@ -74,25 +80,77 @@ class ProductDescriptionPage extends BaseStatelessWidget {
                           ),
                           const SizedBox(height: 3),
                           Text(
-                            'цена: ${product.price} ₴'.toLowerCase(),
+                            'цена: ${product.price} \$'.toLowerCase(),
                             style: AppFonts.size16Regular.copyWith(
                               color: AppColor.white,
                             ),
                           ),
                         ],
                       ),
-                      //TODO: cashback item
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 4,
+                        ),
+                        decoration: BoxDecoration(
+                          color: AppColor.yellow,
+                          // color: Colors.transparent,
+                          border: Border.all(
+                            width: 3,
+                            color: AppColor.yellow,
+                          ),
+                          // border: Border(
+                          //   bottom: BorderSide(
+                          //     width: 3,
+                          //     color: AppColor.yellow,
+                          //   ),
+                          //   top: BorderSide(
+                          //     width: 3,
+                          //     color: AppColor.yellow,
+                          //   ),
+                          //   left: BorderSide(
+                          //     width: 3,
+                          //     color: AppColor.yellow,
+                          //   ),
+                          //   right: BorderSide(
+                          //     width: 3,
+                          //     color: AppColor.blue,
+                          //   ),
+                          // ),
+                          borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(20),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              '${product.cashback}%',
+                              style: AppFonts.size18Bold.copyWith(
+                                color: AppColor.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
                 // WENDER CONTAINER
-                WenderCard(
-                  wendorName: product.store,
-                  typeIcon: product.categoryIcon,
-                  wendorImage: product.wendorImage,
-                  wendorType: product.wendorCategory,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: WenderCard(
+                    wendorType: product.wendorCategory,
+                    wendorImage: product.wendorImage,
+                    typeIcon: product.categoryIcon,
+                    wendorName: product.store,
+                  ),
                 ),
-                const RecomendationList()
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: RecomendationList(),
+                )
               ],
             ),
           ),
